e15f6286cdfb2748ff84ee8bc8f3bfa014c32389
Vo-Tien-Dat <votiendat08112001@gmail.com>
2022-05-12 13:55:04 +0700

    ACM-ICPC

---
+++ Algorithm/Dynamic Programming/LongestIncreasingSubsequence/LongestIncreasingSubsequence.cpp
@@ -0,0 +1,52 @@
+
+
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+/*
+
+
+*/
+
+int main()
+{
+    freopen("LongestIncreasingSubsequence.in", "r", stdin);
+    freopen("LongestIncreasingSubsequence.out", "w", stdout);
+    int n;
+    cin >> n;
+    const int maxn = n + 1;
+    int a[maxn];
+    for (int i = 1; i <= n; ++i)
+    {
+        cin >> a[i];
+    }
+    int LIS[maxn];
+
+    for (int i = 1; i <= n; ++i)
+    {
+        LIS[i] = 1;
+    }
+
+    for (int i = 1; i <= n; ++i)
+    {
+        int ans = 1;
+        for (int j = i; j >= 1; --j)
+        {
+            if (a[i] > a[j])
+            {
+                ans = max(ans, LIS[j] + 1);
+            }
+        }
+        LIS[i] = ans;
+    }
+    int ans = 1;
+    for (int i = 1; i <= n; ++i)
+    {
+        ans = max(LIS[i], ans);
+        cout << LIS[i] << " ";
+    }
+    cout << endl;
+    cout << ans;
+}
